ea379446699b5abf6da1473d18379223c382ce4c
dolearn <dolearn@163.com>
2014-11-06 15:00:22 +0800

    panxunlong add oms.sql

---
+++ oms.sql
@@ -0,0 +1,109 @@
+
+select postcode from  AREA_WH_LOGISTICS_MAPPING where postcode like '%100031%'
+
+update AREA_WH_LOGISTICS_MAPPING set POSTCODE=
+
+ substr(image_file ,1,length(image_file )-2)
+
+drop table test
+create table test(
+    id int primary key not null ,
+    sname varchar(100)
+)
+
+select item_sku from OMS_ORDER_ITEM
+
+
+System.out.println("你哈是的丰盛的是的丰盛的丰盛的发士大夫是的发士大夫sdf="+a);
+
+
+insert into test values ()
+
+select * from OMS_ORDER
+
+
+select * from oms_order_item_presplit 
+
+update OMS_ORDER set oms_order_def_ware_house='SH'
+
+
+
+select * from oms_order_item_presplit where OMS_ORDER_SYS_ID ='8aee819949747984014974837e770024'
+
+
+
+
+8aee81994973ae5b014973c658fd0026
+
+
+select * from oms_product
+167257 8aee82124269f7d701426aea0af10347
+167258 8aee82124269f7d701426aea0afc0348
+
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2235','8aee82124269f7d701426aea0af10347','1',5)
+go
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2236','8aee82124269f7d701426aea0afc0348','2',9)
+go
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2237','8aee82124269f7d701426aea0af10347','2',4)
+go
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2238','8aee82124269f7d701426aea0afc0348','1',1)
+
+select * from OMS_WH_INVENTORY 
+
+
+
+delete from oms_wh_inventory  where OMS_INVENTORY_SYS_ID ='2228'
+
+select * from OMS_WH_INVENTORY where OMS_PRODUCT_SYS_ID ='8aee82124269f7d701426aea07b802ff'
+
+select * from OMS_wh_INVENTORY
+
+select * from OMS_ORDER where SOURCE_ORDER_NO ='19861120'
+
+ 
+select * from oms_order_item where OMS_ORDER_SYS_ID = '8aee81994973ae5b0149745af7ea004c'
+
+--- 131-2200-8708
+
+
+select * from oms_order_item_presplit where OMS_ORDER_SYS_ID ='8aee81994974e6a0014975139cf60060'
+
+8aee81994973ae5b014973c658fd0026
+
+select * from oms_product order by OMS_PRODUCT_SYS_ID desc 
+
+304332 8aee82124269f7d701426aea0af10347
+304331 8aee821546a2a5ad0146c9749dd7515f
+
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2240','8aee821546a2a5ad0146c9749ddf5160','1',5)
+go
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2241','8aee821546a2a5ad0146c9749dd7515f','2',10)
+go
+
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2242','8aee821546a2a5ad0146c9749dd7515f','1',5)
+go
+insert into  OMS_WH_INVENTORY(OMS_INVENTORY_sys_id,oms_product_sys_id,OMS_WH_SYS_ID,INVENTORY)  values('2243','8aee821546a2a5ad0146c9749ddf5160','2',10)
+
+
+ 
+
+select * from OMS_WH_INVENTORY 
+
+
+
+delete from oms_wh_inventory  where OMS_INVENTORY_SYS_ID ='2228'
+
+select * from OMS_WH_INVENTORY where OMS_PRODUCT_SYS_ID ='8aee82124269f7d701426aea07b802ff'
+
+select * from OMS_wh_INVENTORY
+
+select * from OMS_ORDER where SOURCE_ORDER_NO ='19861110'
+
+ 
+select * from oms_order_item where OMS_ORDER_SYS_ID = '8aee81994973ae5b0149745af7ea004c'
+
+
+select * from  INVENTORY_TRANSACTION_LOG  
+
+3cat2car
+
